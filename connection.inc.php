44c8d31f8529fefdf383100d6f432361ef9161f5
Mahendra Telure <teluremahendra@gmail.com>
2022-03-11 16:19:10 +0530

    Update connection.inc.php

--- connection.inc.php
+++ connection.inc.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $con=mysqli_connect("localhost","root","","bolt");
-define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'/online_optical/');
-define('SITE_PATH','http://127.0.0.1/online_optical/');
+define('SERVER_PATH',$_SERVER['DOCUMENT_ROOT'].'https://onlineoptical.herokuapp.com/');
+define('SITE_PATH','https://onlineoptical.herokuapp.com/');
 
 define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
 define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
