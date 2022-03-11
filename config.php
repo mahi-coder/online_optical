f4e5905f2a0ca9cec301be0f26561b1b410b2ebf
Mahendra Telure <teluremahendra@gmail.com>
2022-03-11 15:58:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $currency = '₹';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'bolt';
-$db_host = 'localhost';
+$db_username = 'sql6478330';
+$db_password = 'kCSF7QHzsU';
+$db_name = 'sql6478330';
+$db_host = 'sql6.freemysqlhosting.net';
 $mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
 ?>
